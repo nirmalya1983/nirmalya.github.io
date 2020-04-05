164eec2ebe58f2cc1f5a86f1aeccdbcc5bfd8222
nirmalya1983 <nirmalya1983@gmail.com>
2020-04-05 19:46:32 +0530

    Modified v001

--- js/nirmalya.js
+++ js/nirmalya.js
@@ -1,24 +1,28 @@
-console.log("Here")
-
 $(document).ready(function () {
-	console.log("Here2")
 	var xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = JSON.parse(this.responseText);
-        console.log(myObj);
-		populateVueData(myObj)
+        //console.log(myObj);
+		populateVueData(myObj,stopOnloading)
     }
 	};
 	xmlhttp.open("GET", "https://nirmalya1983.github.io/nirmalya.github.io/js/result.js", true);
 	xmlhttp.send();
 })
-function populateVueData(datain){
+function populateVueData(datain,callback){
 	var example1 = new Vue({
 		el: '#data1',
 		data: {
 		items: datain
 		}
 	})
+	setTimeout(callback,1000)
+}
+function stopOnloading(){
+	$("#onLoading").addClass("d-none");
+	$("#BaseCont").removeClass("d-none");
+	$("body").addClass("loadnormal");
+	$("body").removeClass("loadbg");
 }
 
